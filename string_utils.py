f40bc2691a8e895ea3eeda34be2a88662fd0b229
aleenghanayim132-ops <aleenghanayim132@gmail.com>
2025-12-01 23:15:41 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,26 +1,39 @@
 
 
 
-def split_before_uppercases(formula):
-    pass  # replace the pass with your code
+def split_before_each_uppercases(formula):
+    start = 0
+    end = len(formula) + 1
+    split_formula = []
+    for i in range(start + 1, end):
+      if i == end - 1 or formula[i].isupper():
+        split_formula.append(formula[start:i])
+        start = i
+    return split_formula
 
-def split_at_digit(formula):
-    pass  # replace the pass with your code
+def split_at_first_digit(formula):
+    digit_location = 1
+    for ch in formula[1:]:
+      if ch.isdigit():
+        break
+      digit_location += 1
+    if digit_location == len(formula):
+      return (formula, 1)
+    if digit_location < len(formula):
+      return (formula[:digit_location], int(formula[digit_location:]))
 
 def count_atoms_in_molecule(molecular_formula):
-    """Takes a molecular formula (string) and returns a dictionary of atom counts.  
+    """Takes a molecular formula (string) and returns a dictionary of atom counts.
     Example: 'H2O' → {'H': 2, 'O': 1}"""
-
-    # Step 1: Initialize an empty dictionary to store atom counts
-
-    for atom in split_by_capitals(molecular_formula):
-        atom_name, atom_count = split_at_number(atom)
-        
-        # Step 2: Update the dictionary with the atom name and count
-
-    # Step 3: Return the completed dictionary
-
-
+    formula_dict = {}
+    split_formula = split_before_each_uppercases(molecular_formula) 
+    for i in split_formula:
+        split_i = split_at_first_digit(i)
+        if split_i[0] in formula_dict:
+          formula_dict[split_i[0]] += split_i[1]
+        else:
+          formula_dict[split_i[0]] = split_i[1]
+    return formula_dict
 
 def parse_chemical_reaction(reaction_equation):
     """Takes a reaction equation (string) and returns reactants and products as lists.  
